75a6709634dde3d81f9f4e2a07c2c7fc5fa61c44
vlsrobinson <66073994+vlsrobinson@users.noreply.github.com>
2023-12-05 17:20:48 +0000

    Deploying to gh-pages from @ vlsrobinson/VR_test_Canopy@f08a90846ea324f4e44b3ca8b53b6d7624c12fc4 🚀

---
+++ _next/static/chunks/881.5cb76552961c54ad.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[881],{8881:function(t){t.exports=JSON.parse('{"collection":"https://digitalcollections.lib.washington.edu/iiif/2/korean/manifest.json","featured":["https://cdm16786.contentdm.oclc.org/iiif/korean:138/manifest.json"],"metadata":["Author","Place of Publication"]}')}}]);
